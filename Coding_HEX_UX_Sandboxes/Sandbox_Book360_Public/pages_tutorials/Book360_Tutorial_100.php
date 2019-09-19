66702438a2e4180e4a24fb6169bb4226d02b329a
ivan006 <ivan.copeland2015@gmail.com>
2019-09-19 23:02:03 +0200

    1

---
+++ Coding_HEX_UX_Sandboxes/Sandbox_Book360_Public/pages_tutorials/Book360_Tutorial_100.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tripbuilder</title>
+
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  <meta name="theme-color" content="#088CC6">
+
+</head>
+<!-- Style sheet Start-->
+<style type="text/css">
+
+a
+{
+	color: white;
+
+}
+h1
+{
+	font-family: 
+	 font-size: 100px; 
+	 color: white;
+
+}
+
+h2
+{
+	color: white;
+	font-size: 15px;
+
+
+}
+h3
+{
+	font-size: 15px;
+	color: white;
+}
+
+h4
+{
+	font-size: 60px; 
+	padding-top: 100px; 
+	color: #4169E1;
+}
+
+button
+{
+	color: black;
+	width:auto;
+	height: 50px;
+	font-style: bold;
+	background-color: transparent;
+	color: white;
+	border-color: white;
+	border-style: round;
+}
+
+header
+{
+background-image:url("../assets_images/Gradient.jpg");
+padding-bottom: 50px;
+
+}
+
+ul
+{
+	 font-size: 20px;
+}
+p
+{
+	 font-size: 20px; 
+	 color: black;
+}
+
+</style>
+
+<!-- Style Sheet End-->
+
+<header>
+<!--Menu Start-->	
+<div class="container">
+
+<!-- First row Start-->
+<div class="row">
+
+<div class ="col-md-4">
+<h4><a href ="https:\\Book360.io">Book360</a></h4>
+</div>
+<div class="col-md-2">
+<h2><b>Features</b></h2>
+<h3><a href="https://www.book360.io/cookbook/Coding_HEX_UX_Sandboxes/Sandbox_Book360_Public/pages_tutorials/Book360_Tutorial_100.php">Tripbuilder</a></h3>
+<h3>Docbuilder</h3>
+<h3>TravelGuide</h3>
+<h3>Dashboard</h3>
+</div>
+
+
+<div class="col-md-2">
+<h2><b>How it works</b></h2>
+<h3>Consultation</h3>
+<h3>Implementation</h3>
+<h3>Configuration</h3>
+<h3>Training</h3>
+<h3>Customisaion</h3>
+<h3>Licensing</h3>
+
+</div>
+
+<div class="col-md-2">
+<h2><b>Help and Support</b></h2>
+<h3>Support Team</h3>
+<h3>Tutorials</h3>
+<h3>FAQ's</h3>
+<h3>GDPR</h3>
+</div>
+<div class="col-md-2">
+<h2><b>Contact us</b></h2>
+
+<h3>+27 (0)21 418 0746</h3>
+<h3>+385 (0)91 947 1279</h3>
+<h3>capetown@book360.io</h3>
+<h3>sutican@book360.io</h3>
+<h3>twitter.com/book360.io</h3>
+<h3>instagram.com/book360.io</h3>
+
+</div>
+</div>
+</div>
+
+<div class="container">
+<div class ="row">
+<div class="col-md-6">
+<h1>The Tripbuilder</h1>
+<p>The TripBuilder tool is used by consultants to
+design TravelGuides from scratch or based
+upon pre-costed packages.</pre>
+</div>
+<div class="col-md-6">
+<img src="../assets_images/Tripbuilder_Illustration_03_.png">
+</div>
+</div>
+
+<!-- First Row End-->
+
+<!-- Button Row Start -->
+<div class="row">
+<div class="col-md-6">
+<button>Read more</button>
+</div>
+
+<div class="col-md-6">
+<button>Learn more about us</button>
+</div>
+</div>
+
+<!-- Button Row End -->
+
+<!-- Menu End-->
+</header>
+
+<body>
+
+</div>
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<h4>What is The Tripbuilder</h4>
+<p>The TripBuilder tool is used by consultants to design TravelGuides from scratch or based upon pre-costed packages.A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box. Quick brown dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard’s job is to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq</p>
+</div>
+
+</div>
+<div class="row">
+<div class="col-md-12">
+
+<h4>TripBuilder does the following functions</h4>
+<ul>
+	<li>Design, plan and create itinaries/packages.</li>
+	<li>Enables a travel consultant to check availability and secure reservations.</li>
+	<li>Enables the travel consultant to create quotes for the travel agent (B2B)/traveller(B2C).</li>
+	<li>Powers the TravelGuide mobile app, the TravelGuide web app and the TravelGuide PDF.</li>
+	<li>Enables the ability to create invoices through the Docbuilder.</li>
+</ul>
+</div>
+</div>
+
+<div class ="row">
+<div class="col-md-12">
+<h4>How to Access the Tripbuilder:</h4>
+
+<ul>
+
+<li>Click on Tripbuilder Bookings and then select My Bookings</li>
+<li>Click on "Create a new booking" and select the "Create a new booking from scratch"</li>
+<li>Start the booking process by clicking on <b>Dates</b> and select the amount of days for the trip</li>
+<li>Click on the <b>Edit trip information</b> button, it is located at the top right corner of the landing page.<br> Here you must insert the Party name, Agent (click on the drop-down and select the agent) Budget and the Status  (click on the drop-down and select the status) of the booking on this window. To save the information you have filled in click on “Save”.</li>
+<li>Click on "<b>Pax Configuration</b>" button, it is located at the top right corner of the landing page and then the “Add room“ button. Here you must insert the Name of the room, Room type (click on the drop-down and select the room type) and add the amount of adults, children and infants (click on “Add adult”, “Add child” and “Add infant”). To save the information you have filled in click on “Save room details”.</li>
+<li>After you have selected all the necessary services click on “ Save Booking”.</li>
+<li>Once you have saved the booking it will generate the “ABFI” reference number and you will  have the option to either view the booking summary or continue editing in the TripBuilder.</li>
+</ul>
+</div>
+</div>
+
+
+<div class="row">
+<div class="col-md-12">
+<h4>Accommodation</h4>
+<p>Accommodation is a room, a group of rooms, or a building in which someone may live or stay.</p>
+<ul>
+<li>Destination - click on the dropdown and select the area in which the accommodation is situated.<small>E.g. Botswana.</small></li>
+<li>Supplier - type in the name of the accommodation. <small>E.g. Holiday Inn Express.</small></li>
+<li>Address - will automatically be inserted by the system. </li>
+<li>Room type - the room type will automatically be inserted once you have chosen the accommodation.</li>
+<li>Check in & out - select the check out and check in dates from the calendar.</li>
+</ul>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-12">
+<h4>Excursion</h4>
+<p>An excursion is a short journey or trip, especially one taken as a leisure activity.</p>
+<ul>
+<li>Destination - click on the dropdown and select the area in which the excursion is situated. <small>E.g. Cape Town CBD</small></li>
+<li>Supplier - type in the name of the excursion.<small>E.g. City Sightseeing Cape Town.</small></li>
+<li>Address - will automatically be inserted by the system.</li>
+<li>Excursion type - will automatically be inserted by the system.</li>
+<li>Pick up & Drop off - select the check out and check in dates from the calendar. At the bottom of the calendar you will find the dropdown to select the time for the pick up and drop off.</li>
+</ul>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-12">
+<h4>Meet and Greet</h4>
+<p>A Meet & Greet, is usually in the arrivals hall at the airport, where the driver meets the traveller(s) and assist them with their luggage and answers any questions they may have, then the driver will transfer them to their accommodation.</p>
+
+<ul>
+<li>Destination - click on the dropdown and select the area in which the driver has to collect the traveller.<small>E.g. Gauteng Pretoria.</small></li>
+<li>Supplier - type in the name of the company that will be meeting the traveller at the location.<small>E.g. African Ample Assistance.</small></li>
+<li>Physical address - will automatically be inserted by the system.</li>
+<li>Service description - will automatically be inserted by the system.</li>
+<li>Pick up time - select the pick up date from the calendar. At the bottom of the calendar you will find the dropdown to select the time for the pick up.</li>
+</ul>
+</div>
+</div>	
+
+<div class="row">
+<div class="col-md-12">
+<h4>Car Rental</h4>
+<p>A car rental is a company that rents out vehicles for a short period of time for a certain fee depending on the category and class of the vehicle, as well as the amount of days the vehicle will be out for rental. There are also multiple insurances that can be taken out while the vehicle is in the possession of the customer. </p>
+<ul>
+<li>Supplier - type in the name of the company you would like to rent a car from. <small>E.g. Bidvest.</small></li>
+<li>Pick up location - the place where the traveller has to pick up the car. <small>E.g. BedFordView Eastgate.</small></li>
+<li>Drop off location - the place where the traveller has to drop off the car.<small>E.g. Claremont, Cavendish Square.</small></li>
+<li>Service description - will automatically be inserted by the system.</li>
+<li>Pick up time - select the pick up date from the calendar. At the bottom of the calendar you will find the dropdown to select the pick up time.</li>
+<li>Drop off time - select the drop off date from the calendar. At the bottom of the calendar you will find the dropdown to select the drop off time.</li>
+</ul>
+</div>
+</div>
+
+
+<div class="row">
+<div class="col-md-12">
+<h4>Transfer</h4>
+<p>A transfer is when the traveller meets the driver at the arrivals / info desk at the airport or at the pick-up zone outside of the airport. Travellers find their own way from the arrivals hall to the transfer.</p>
+
+<ul>
+<li>Destination - click on the dropdown and select the area in which the driver has to collect the traveller.<small>E.g. Free State.</small></li>
+<li>Supplier - the name of the company that will collect the traveller from the destination. </li>
+<li>Physical address - will automatically be inserted by the system.</li>
+<li>Service description - will automatically be inserted by the system.</li>
+<li>Pick up time - select the pick up date from the calendar. At the bottom of the calendar you will find the dropdown to select the pick up time.</li>
+</ul>
+</div>
+</div>
+
+
+
+<div class="row">
+<div class="col-md-12">
+<h4>Entrance Fee</h4>
+<p>An entrance fee is a sum of money which you pay before you go somewhere such as a cinema or museum, or which you have to pay in order to join an organization or institution.</p>
+<ul>
+<li>Destination - click on the dropdown and select the area in which the traveller will need to pay a fee.<small>E.g. Bloemfontein.</small></li>
+<li>Supplier - the name of the where the traveller has to pay a fee.<small>E.g. Parking fee at Parliament Hotel.</small></li>
+<li>Physical address - will automatically be inserted by the system.</li>
+<li>Service description - will automatically be inserted by the system.</li>
+<li>Pick up time - select the pick up date from the calendar.</li>
+</ul>
+</div>
+</div>
+
+
+
+<div class="row">
+<div class="col-md-12">
+<h4>Flight</h4>
+<p>A flight is the action or process od flying through the air with an aeroplane/airplan</p>
+<ul>
+
+<li>Supplier - the name of the airline the traveller will be using.</li>
+<li>Departure -  the city where the traveller will be departing.</li>
+<li>Arrival - the city where the traveller will be arriving.</li>
+<li>Service description - will automatically be inserted by the system.</li>
+<li>Departure Time</li>
+<li>Arrival Time</li>
+</ul>
+</div>
+</div>
+
+
+<div class ="row">
+<div class ="col-md-12">
+<h4>Meals</h4>
+
+<p> A meal is the food served and eaten at regular occasions for taking food during the day such as breakfast, lunch and supper.</p>
+
+<ul>
+<li>Destination -  Click on the dropdown and select the area in which the driver has to collect the traveller.<small>E.g. Cape Boland</small> </li>	
+<li>Supplier - The name of the establishment where the traveller will be having the meal. <small>E.g. Capetonian Hotel</small></li>
+<li>Physical Address - Will automatically be inserted by the system.</li>
+<li>Service Description - Will automatically be inserted by the system.</li>
+<li>Meal Time - Select the date and time on the Calendar</li>
+</ul>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-12">
+<h4>Create a new booking from a package:</h4>
+<ul>
+<li>Click on “TripBuilder Bookings” and then select “My Bookings”.</li>
+<li>Click on “Create a new booking” and select the “Create a new booking from a package”. </li>
+<li>Start the booking process by selecting a package from the grid.</li>
+<li>Fill in the traveller(s) name and select the agent from the dropdown.</li>
+<li>Click on “Create new booking” to proceed with the booking.</li>
+<li>Once you have clicked on “Create new booking”, a window will pop up with the “ABFI” reference number for the booking. You will also have the option to either view the booking or continue to edit.</li>
+</ul>
+
+
+</div>
+</div>
+
+
+
+<div class="row">
+<div class="col-md-12">
+
+<h4>Cost breakdown</h4>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost</p>
+</div>
+</div>
+
+
+<div class="row">
+<div class="col-md-12">
+<h4>Docbuilder Integration</h4>
+<p>A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box. Quick brown dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard’s job is to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq</p>
+</div>
+</div>
+
+
+
+<div class="row">
+<div class="col-md-12">
+<h4>Pax Configuration</h4>
+<p>A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box. Quick brown dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard’s job is to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq</p>
+</div>
+</div>
+</div>
+<!-- Third Row End-->
+
+</div>
+
+
+
+
+
+</body>
+</html>
